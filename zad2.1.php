8c568a8116c78ea672e21cc81e41e8e95c9c473f
Tlesikowski <126799652+Tlesikowski@users.noreply.github.com>
2023-03-13 08:48:05 +0100

    Update zad2.1.php

--- zad2.1.php
+++ zad2.1.php
@@ -4,12 +4,17 @@ function rantom(){
   
   $index =0;
   $naray = array();
+  
+  for($i = 0;$i < 20;$i++) {
+            $naray[$i] = rand(1, 50);
+        }
+  echo "Wybierz indeks:(0-19) ";
+        $index = readline();
+  return $naray[$index];
 }
 
-
-
-
-
+$ans= rantom();
+echo $ans;
 
 
 ?>
